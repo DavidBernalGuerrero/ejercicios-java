79d82648b3674126deb57a3f9b08ce118f788160
DavidBernalGuerrero <davidbg1992@hotmail.com>
2015-10-01 11:18:13 +0200

    Actualización de ejercicios del capitulo 3.

--- tema3/T3E9PideRadioAlturaCalculaAreaDeCono.java
+++ tema3/T3E9PideRadioAlturaCalculaAreaDeCono.java
@@ -19,7 +19,7 @@
     System.out.printf("\nEl area del cono, siendo su radio %.2fcm² y su altura "
       , radio);
     System.out.printf("%.2fcm², calcularemos el área del cono: %.2f", altura,
-      ((3.141516*radio*radio*altura)/3));
+      ((Math.PI*radio*radio*altura)/3));
       
     }
   }
