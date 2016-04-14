ea3f0e9bff0b8991f8319a29e87b600102bd2cbb
David Bernal Guerrero <davidbg1992@hotmail.com>
2016-04-15 00:19:13 +0200

    Added files via upload

---
+++ tema09/Mamifero.java
@@ -0,0 +1,51 @@
+
+package ArrayPOOEj01Cap09;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Mamifero extends Animal {
+  
+  public Mamifero (String edad, String sexo, String gestacion) {
+    this.edad = edad;
+    this.sexo = sexo;
+    this.gestacion = gestacion;
+  }
+
+  public String getSexo() {
+    return sexo;
+  }
+
+  public void setSexo(String sexo) {
+    this.sexo = sexo;
+  }
+
+  public String getEdad() {
+    return edad;
+  }
+
+  public void setEdad(String edad) {
+    this.edad = edad;
+  }
+
+  public String getGestacion() {
+    return gestacion;
+  }
+
+  public void setGestacion(String gestacion) {
+    this.gestacion = gestacion;
+  }
+  
+  void corre() {
+    System.out.println("1,2,3...1,2,3...");
+  }
+  
+  void creaMadriguera() {
+    System.out.println("Esta será mi futura casa.");
+  }
+  
+  void lame () {
+    System.out.println("Tengo que asear a mis crias.");
+  }
+}
